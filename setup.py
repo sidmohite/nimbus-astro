41ee0ad8101aee97ddcdec10c24be895929908fe
Siddharth Mohite <srmohite@uwm.edu>
2021-07-11 11:47:05 -0500

    Several updates at once

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     # Needed to silence warnings (and to be a worthwhile package)
-    name='nimbus',
+    name='nimbus-astro',
     url='https://github.com/sidmohite/nimbus-astro',
     author='Siddharth Mohite',
     author_email='srmohite@uwm.edu',
@@ -17,11 +17,11 @@ setup(
     # Needed for dependencies
     install_requires=['numpy','scipy','astropy','pandas'],
     # *strongly* suggested for sharing
-    version='0.1',
+    version='1.0.0',
     # The license can be anything you like
     license='MIT',
-    description='A hierarchical Bayesian inference framework to 
-        constrain kilonova population properties.',
+    description='A hierarchical Bayesian inference framework to constrain\
+	       kilonova population properties.',
     # We will also need a readme eventually (there will be a warning)
     long_description=long_description,
     long_description_content_type='text/markdown',
