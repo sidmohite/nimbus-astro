57239acab1a38774b4568b9cd410c5a7278c0f18
Siddharth Mohite <srmohite@uwm.edu>
2021-07-12 11:45:08 -0500

    Convert executables to python scripts

--- nimbus/scripts/combine_fields.py
+++ nimbus/scripts/combine_fields.py
@@ -30,7 +30,11 @@ parser.add_argument('--P_A', required = True, type = float,
                    astrophysical.Range(0-1)')
 parser.add_argument('--output_file', required = True, metavar = 'OUTPUT_FILE',
                    help = 'Output file.')
+
 args = parser.parse_args()
+P_A = args.P_A
+coverage_fraction = args.coverage_fraction
+output_file = args.output_file
 
 def combine_infield_likelihoods(infield_files):
     infield_likelihoods = np.array([np.loadtxt(field_file) for field_file
@@ -45,15 +49,17 @@ def lnposterior_covfrac(likelihoods, P_f, cov_frac, P_A, P_T):
     return np.log((likelihoods*cov_frac/np.sum(P_f) + (1 - cov_frac))*P_A +\
                  P_T)
 
-infield_files = np.sort(glob(args.infield_likelihoods_str+'*.txt'))
-samples = np.loadtxt(args.sample_file)
-likelihoods = combine_infield_likelihoods(infield_files)
-P_f = np.loadtxt(args.field_probs_file)
+def main(likelihoods, P_f, P_A, coverage_fraction, samples, output_file):
+    if coverage_fraction:
+        ln_posterior = lnposterior_covfrac(likelihoods, P_f, coverage_fraction,
+                                          P_A, 1.- P_A)
+    else:
+        ln_posterior = lnposterior_allsky(likelihoods, P_f, P_A, 1.- P_A)
+    np.savetxt(output_file,np.c_[samples,ln_posterior])
 
-if args.coverage_fraction:
-    ln_posterior = lnposterior_covfrac(likelihoods, P_f,
-                                      args.coverage_fraction, args.P_A,
-                                      1.-args.P_A)
-else:
-    ln_posterior = lnposterior_allsky(likelihoods, P_f, args.P_A, 1.-args.P_A)
-np.savetxt(args.output_file,np.c_[samples,ln_posterior])
+if __name__ == "__main__":
+    infield_files = np.sort(glob(args.infield_likelihoods_str+'*.txt'))
+    samples = np.loadtxt(args.sample_file)
+    likelihoods = combine_infield_likelihoods(infield_files)
+    P_f = np.loadtxt(args.field_probs_file)
+    main(likelihoods, P_f, P_A, coverage_fraction, samples, output_file)
--- nimbus/scripts/compute_field_probs.py
+++ nimbus/scripts/compute_field_probs.py
@@ -30,15 +30,26 @@ parser.add_argument('--common_str', required = True,
                    help = 'The common string pattern (see below) the code\
                    expects the files to be named with.')
 args = parser.parse_args()
+survey_file = args.survey_file
+infield_likelihoods_path = args.infield_likelihoods_path
+skymap_file = args.skymap_file
+common_str = args.common_str
+field_probs_file = args.field_probs_file
 
-df_survey = pd.read_pickle(args.survey_file)
+def main(survey_file, skymap_file, field_probs_file, infield_likelihoods_path,
+        common_str):
+    df_survey = pd.read_pickle(survey_file)
 
-infield_likelihood_files = np.sort(glob(args.infield_likelihoods_path))
-field_probs = np.zeros(len(infield_likelihood_files))
-skymap_prob = sky_utils.Skymap_Probability(skymap_fits_file=args.skymap_file)
-for i,field_file in enumerate(infield_likelihood_files):
-    field_num = int(field_file.split(args.common_str)[1].split('.')[0])
-    ipix_field = df_survey.ipix.values[df_survey.field_ID==field_num][0]
-    field_probs[i] = skymap_prob.calculate_field_prob(ipix_field)
+    infield_likelihood_files = np.sort(glob(infield_likelihoods_path))
+    field_probs = np.zeros(len(infield_likelihood_files))
+    skymap_prob = sky_utils.Skymap_Probability(skymap_fits_file=skymap_file)
+    for i,field_file in enumerate(infield_likelihood_files):
+        field_num = int(field_file.split(common_str)[1].split('.')[0])
+        ipix_field = df_survey.ipix.values[df_survey.field_ID==field_num][0]
+        field_probs[i] = skymap_prob.calculate_field_prob(ipix_field)
 
-np.savetxt(args.field_probs_file,field_probs)
+    np.savetxt(field_probs_file,field_probs)
+
+if __name__ == "__main__":
+    main(survey_file, skymap_file, field_probs_file, infield_likelihoods_path,
+        common_str)
